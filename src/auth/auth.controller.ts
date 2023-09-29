66874f6a96fdff1b0a26a334ce168dd38b3abd25
Pankaj Kumar <pankajs2167@gmal.com>
2023-09-29 09:31:49 +0530

    version 0.0

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller,Post,Req,ParseIntPipe, HttpCode, HttpStatus } from "@nestjs/common";
+import { Request } from 'express';
+import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto/auth.dto";
+
+@Controller('auth')
+export class AuthController{
+    constructor(private authService:AuthService){}
+        @Post('signup')
+        signup(@Body() dto:AuthDto){
+            return this.authService.signup(dto);
+
+        }
+        @HttpCode(HttpStatus.OK)
+        @Post('signin')
+        signin(@Body() dto:AuthDto){
+            return this.authService.signin(dto);
+        }
+    
+}
---
+++ src/user/user.controller.ts
@@ -0,0 +1,25 @@
+import { Body, Controller,Get, Patch, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from '@prisma/client';
+import { Request } from 'express';
+import { GetUser } from '../auth/decorator/get-user.decorator';
+import { JwtGuard } from '../auth/guard/jwt.guard';
+import { EditUserDto } from './dto';
+import { UserService } from './user.service';
+
+@Controller('users')
+@UseGuards(JwtGuard)
+export class UserController {
+    constructor(private userService:UserService){}
+    @Get('me')
+    getMe(  @GetUser() user:User){    
+        return user;
+    }
+
+    @Patch()
+    editUser(@GetUser('id') userId:number,
+    @Body() dto:EditUserDto,
+    ){
+        return this.userService.editUser(userId,dto);
+    }
+}
